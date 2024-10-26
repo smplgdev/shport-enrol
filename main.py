46f4d9728db14e9e9cb4d85c578a62d107481695
anna miletova <annamiletova@gmail.com>
2024-10-26 17:39:58 +0200

    Telegram implemented

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import os
 import re
 import requests
 import time
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import asyncio
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from datetime import datetime
 
-# has places
-WEBSITE_URL="https://sportup.si/dogodki/skupinski-tek-group-jogging-2/?occurrence=2024-10-28"
+load_dotenv()
 
-# no places available
-# WEBSITE_URL="https://sportup.si/dogodki/vadba-za-moc-strength-training-624/?occurrence=2024-10-28"
+TG_BOT_API_TOKEN = os.getenv("TG_BOT_API_TOKEN")
 
-# registration not open
-# WEBSITE_URL="https://sportup.si/dogodki/plezanje-indoor-climbing/?occurrence=2024-10-29"
+bot = Bot(TG_BOT_API_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
+
+dp = Dispatcher()
 
 def fetch_page(website_url):
     response = requests.get(website_url)
@@ -20,38 +28,68 @@ def fetch_page(website_url):
 
     return BeautifulSoup(response.content, 'html.parser')
 
+
 def check_places_available(booking_container):
     if not any("mec-util-hidden" in s for s in booking_container['class']):
         return False 
     return True
 
-def main(website_url=None):
-    if not website_url:
-        website_url = input("Enter event link: ")
+def check_link(link):
+    return re.match(r"https://sportup.si/dogodki/.*", link)
 
+async def start_tracking(message):
     is_booking_available = False
 
-    while(True):
-        bs = fetch_page(website_url)
+    bs = fetch_page(message.text)
+
+    if not bs:
+        return
+
+    event_name = bs.title.string.split("-")[0].strip()
+    await message.answer("Started tracking event " + event_name)
+
+    while True:
+        bs = fetch_page(message.text)
+
+        print("Tracking... " + datetime.now().strftime("%H:%M:%S"))
 
         if not bs:
             return
 
+        event_name = bs.title.string.split("-")[0].strip()
+
         booking_container = bs.find(id=re.compile("^mec-events-meta-group-booking-[0-9]+$"))
 
         if not booking_container:
-            print("Registration not open yet")
             is_booking_available = False
         elif check_places_available(booking_container.find(class_="mec-ticket-unavailable-spots")):
-            print("Places available")
             if (is_booking_available == False):
                 is_booking_available = True
-                # send notification
+                await message.answer(f"Booking available for {event_name}! {message.text}")
         else:
-            print("No places available")
             is_booking_available = False
         
         time.sleep(60)
 
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Hello! Send me the link to the event you want to track")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    try:
+        if check_link(message.text):
+            await start_tracking(message)
+        else:
+            await message.answer("Invalid link")
+    except TypeError:
+        await message.answer("Unsupported message type")
+
+
+async def main():
+    await dp.start_polling(bot)
+
 if __name__ == "__main__":
-    main(WEBSITE_URL)
+    asyncio.run(main())
