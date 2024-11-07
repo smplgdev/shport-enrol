7b0e05ef5c66c553404f290810686a6dd3bd5fbd
smplgdev <73465772+smplgdev@users.noreply.github.com>
2024-11-07 20:12:26 +0100

    Add .env RELOAD_TIMEOUT_SECONDS variable

--- services/tracking.py
+++ services/tracking.py
@@ -1,14 +1,18 @@
 import asyncio
 import logging
+import os
 import re
-from datetime import datetime
+
+from aiogram.types import Message
 
 from services.api import fetch_page, check_places_available, fetch_page_title
 
 logger = logging.getLogger(__name__)
 
+RELOAD_TIMEOUT_SECONDS = os.getenv("RELOAD_TIMEOUT_SECONDS", 60)
 
-async def start_tracking(message):
+
+async def start_tracking(message: Message):
     try:
         event_name = fetch_page_title(message.text)
     except ValueError as e:
@@ -20,7 +24,7 @@ async def start_tracking(message):
     while True:
         bs = fetch_page(message.text)
 
-        logger.info("Tracking... " + datetime.now().strftime("%H:%M:%S"))
+        logger.info("Tracking... ")
 
         booking_container = bs.find(id=re.compile("^mec-events-meta-group-booking-[0-9]+$"))
 
@@ -28,4 +32,4 @@ async def start_tracking(message):
             await message.answer(f"Booking available for {event_name}! {message.text}")
             break
 
-        await asyncio.sleep(60)
+        await asyncio.sleep(RELOAD_TIMEOUT_SECONDS)
